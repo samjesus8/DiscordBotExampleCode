4b864cb8b3fb77a6ef86a07bda0296b9e91364fd
samjesus8 <samueljesuthas27@gmail.com>
2023-12-31 01:52:40 +0000

    Add project files.

---
+++ Config/ConfigJsonReader.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace DiscordBotTutorialExampleProject.Config
+{
+    public class ConfigJsonReader
+    {
+        //Declare our Token & Prefix properties of this class
+        public string discordToken { get; set; }
+        public string discordPrefix { get; set; }
+
+        public async Task ReadJSON() //This method must be run asynchronously
+        {
+            //Initialise a StreamReader
+            using StreamReader sr = new("config.json", new UTF8Encoding(false));
+
+            //Read the JSON file
+            string json = await sr.ReadToEndAsync();
+
+            //Deserialize into a JSONStruct object
+            JSONStruct obj = JsonConvert.DeserializeObject<JSONStruct>(json);
+
+            //Set the properties
+            this.discordToken = obj.token;
+            this.discordPrefix = obj.prefix;
+        }
+    }
+
+    internal sealed class JSONStruct
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DiscordBotTutorialExampleProject
+{
+    //Make sure the Program class is sealed so that nothing can inherit it
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
