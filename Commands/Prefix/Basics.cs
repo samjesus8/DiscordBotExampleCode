be549fd136c5c1f2cb83105e55005dd508a196f4
samjesus8 <samueljesuthas27@gmail.com>
2023-12-31 02:22:53 +0000

    Added Basics

---
+++ Commands/Prefix/Basics.cs
@@ -0,0 +1,68 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace DiscordBotTutorialExampleProject.Commands.Prefix
+{
+    //Every Command class must be PUBLIC and must inherit BaseCommandModule
+    public class Basics : BaseCommandModule
+    {
+        //Use the Command attribute to declare a command
+        [Command("test")]
+
+        //Then create an async method for the command. It MUST have the CommandContext as its 1st parameter
+        public async Task MyFirstCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello World!");
+        }
+
+        [Command("embed")]
+        public async Task EmbedMessageExamples(CommandContext ctx)
+        {
+            //Using a DiscordMessageBuilder
+            var embedMessage1 = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Azure)
+                    .WithTitle("Embed Message Title")
+                    .WithDescription("Embed Message Description"));
+
+            //Using a DiscordEmbedBuilder
+            var embedMessage2 = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Azure,
+                Title = "Embed Message Title",
+                Description = "Embed Message Description"
+            };
+
+            await ctx.Channel.SendMessageAsync(embedMessage1);
+            await ctx.Channel.SendMessageAsync(embed: embedMessage2);
+        }
+
+        [Command("calculator")]
+        public async Task CommandParametersExample(CommandContext ctx, int num1, string operation, int num2)
+        {
+            int result = 0;
+
+            switch(operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    await ctx.Channel.SendMessageAsync("Please enter a valid operation (+, -, *, /)");
+                    break;
+            }
+
+            await ctx.Channel.SendMessageAsync(result.ToString());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,73 @@
-﻿namespace DiscordBotTutorialExampleProject
+﻿using DiscordBotTutorialExampleProject.Commands.Prefix;
+using DiscordBotTutorialExampleProject.Config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.SlashCommands;
+
+namespace DiscordBotTutorialExampleProject
 {
     //Make sure the Program class is sealed so that nothing can inherit it
     public sealed class Program
     {
-        static void Main(string[] args)
+        public static DiscordClient Client { get; set; }
+        public static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
         {
-            
+            //1. Retrieve Token/Prefix from config.json
+            var configProperties = new ConfigJsonReader();
+            await configProperties.ReadJSON();
+
+            //2. Create the Bot Configuration
+            var discordConfig = new DiscordConfiguration
+            {
+                Intents = DiscordIntents.All,
+                Token = configProperties.discordToken,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            //3. Initialise the DiscordClient property
+            Client = new DiscordClient(discordConfig);
+
+            //Set defaults for interactivity based events
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            //4. Set up the Ready Event-Handler
+            Client.Ready += Client_Ready;
+
+            //5. Create the Command Configuration
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configProperties.discordPrefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false
+            };
+
+            //6. Initialize the CommandsNextExtention property
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            //Enabling the Client to use Slash Commands
+            var slashCommandsConfig = Client.UseSlashCommands();
+
+            //7. Register your Command Classes
+            Commands.RegisterCommands<Basics>();
+
+            //8. Connect the Client to the Discord Gateway
+            await Client.ConnectAsync();
+            //Make sure you delay by -1 to keep the bot running forever
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
